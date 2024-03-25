7423903821e07e02d827e20c4492c76835da085e
SaranjeetSingh19 <ss5259603@gmail.com>
2024-03-25 12:52:54 +0530

    Final Deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://boss-app-e0vg.onrender.com", {
         query: {
           userId: authUser._id,
         },
